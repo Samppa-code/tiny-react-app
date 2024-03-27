cae9a66a91de7bef64b9ead64a98eb72a036ab1d
Samu Nordman <nordman.samu@gmail.com>
2024-03-27 12:06:12 +0200

    Added lint

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,17 @@
-// src/Button.js
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Button;
