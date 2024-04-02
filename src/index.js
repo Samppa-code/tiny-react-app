2a1d214708d509b0503bc730918714787860aa25
Samu Nordman <nordman.samu@gmail.com>
2024-04-02 15:59:08 +0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React  from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Counter />
+    </React.StrictMode>,
+    document.getElementById("root")
+
+);
